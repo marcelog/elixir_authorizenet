e98a11d411894a396ac3197c9120e6aea338aebf
Marcelo Gornstein <marcelog@gmail.com>
2015-10-28 09:49:05 -0300

    removing stale credentials

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :elixir_authorizenet,
   environment: :sandbox,
   validation_mode: :test, # :test, :live, or :none
-  login_id: "77yyV5KGJf",
-  transaction_key: "674VaGBSe5Eh58Sw"
+  login_id: "login_id",
+  transaction_key: "transaction_key"
 
 import_config "#{Mix.env}.exs"
