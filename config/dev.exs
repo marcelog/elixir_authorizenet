5f14a5003fc3c78ba20b30eb8fda62d4093a3666
Marcelo Gornstein <marcelog@gmail.com>
2015-10-28 09:48:37 -0300

    removing cfg file

--- config/dev.exs
+++
@@ -1,7 +0,0 @@
-use Mix.Config
-
-config :elixir_authorizenet,
-  environment: :production,
-  validation_mode: :live, # :test, :live, or :none
-  login_id: "5D494aLsBnn2",
-  transaction_key: "26x8s7LQrv28MPyZ"
