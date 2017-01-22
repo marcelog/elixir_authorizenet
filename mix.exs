6e329572dc58d9726e68a370527474611c9335db
Marcelo Gornstein <marcelog@gmail.com>
2017-01-22 15:18:23 -0300

    reducing description

--- mix.exs
+++ mix.exs
@@ -25,14 +25,12 @@ defmodule AuthorizeNet.Mixfile do
 
   defp description do
     """
-Elixir client for the Authorize.Net merchant API. This should help you integrate using the AIM.
+Elixir client for the Authorize.Net merchant AIM API.
 
-A nice number of features are implemented (probably most of the ones used
-on a daily basis are already there), but since the API offers a big number of
-features and combinations, I still consider this as WIP, and pull requests,
+A number of features are implemented, but I still consider this as WIP, and pull requests,
 suggestions, or other kind of feedback are very welcome!
 
-Find the user guide in the github repo at: https://github.com/marcelog/elixir_authorizenet.
+User guide at: https://github.com/marcelog/elixir_authorizenet.
     """
   end
 
